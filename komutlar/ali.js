9eb13f73bb2d05867193836fa02ad7077c38b6a2
emiiirrrr <79011901+emiiirrrr@users.noreply.github.com>
2021-02-16 20:24:47 +0300

    Update ali.js

--- komutlar/ali.js
+++ komutlar/ali.js
@@ -3,6 +3,7 @@ const { Channel } = require("discord.js")
 module.exports = {
     kod:'ali',
     async run (client, message, args) {
+      if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply('Bunu yapamazsın! ❌')
         message.channel.send('<@780210505884499990> <@780210505884499990> <@780210505884499990> <@780210505884499990> <@780210505884499990> <@780210505884499990> <@780210505884499990> <@780210505884499990> <@780210505884499990> <@780210505884499990> <@780210505884499990>')
     }
 }
